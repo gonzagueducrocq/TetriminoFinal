a78059a7134d1f9b7c3e3f2108968834385be9dc
Jeremy PERROUAULT <jeremy.perrouault@ascadis.fr>
2017-09-11 11:49:03 +0200

    Coquille URL API

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 var app = angular.module("tpAngular", [ 'ngRoute', 'ngResource' ]);
 
-app.constant("API_URL", "http://localhost8080/tetrimino/api");
+app.constant("API_URL", "http://localhost:8080/tetrimino/api");
